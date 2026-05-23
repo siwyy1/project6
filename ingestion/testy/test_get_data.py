5aaca13589af2fe0cf471de166c1d17db8bdd806
Siwy <lxqd@wp.pl>
2026-05-23 12:08:46 +0200

    wojowanie

--- ingestion/testy/test_get_data.py
+++ ingestion/testy/test_get_data.py
@@ -1,5 +1,21 @@
 from ingestion.get_data import *
-def test_fetch():
-    data = fetch()
-    assert isinstance(data, list)
+from unittest.mock import patch, Mock
+import json
 
+@patch("ingestion.get_data.requests.get")
+def test_fetch(mock_get):
+
+    fake_data = [
+        {"name": "repo1"},
+        {"name": "repo2"}
+    ]
+
+    mock_response = Mock()
+    mock_response.json.return_value = fake_data
+
+    mock_get.return_value = mock_response
+
+    data = fetch()
+
+    assert isinstance(data, list)
+    assert data == fake_data
